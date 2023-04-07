d73e57a9d7343daa8c43cc9c3b1c38897112c746
Douglas Wu <wckdouglas@gmail.com>
2023-04-06 22:08:08 -0400

    added ci

--- src/distance.rs
+++ src/distance.rs
@@ -12,3 +12,30 @@ pub fn hamming_distance(barcode_a: &[u8], barcode_b: &[u8]) -> Result<u8, String
         Err("Barcode lengths are not the same".to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[test]
+    #[should_panic(expected = "Barcode lengths are not the same")]
+    fn test_hamming_distance_fail() {
+        let str_a = "ACTGCGG".as_bytes();
+        let str_b = "ACTGCG".as_bytes();
+        hamming_distance(&str_a, &str_b).unwrap();
+    }
+
+    #[rstest]
+    #[case("ACTG", "ACTG", 0)]
+    #[case("ACTG", "ATTG", 1)]
+    #[case("ACTG", "ATGG", 2)]
+    #[case("ACTG", "ATCT", 3)]
+    #[case("ACTG", "GACT", 4)]
+    fn test_hamming_distance(#[case] str_a: &str, #[case] str_b: &str, #[case] expected_score: u8) {
+        let str_a_bytes = str_a.as_bytes();
+        let str_b_bytes = str_b.as_bytes();
+        let score = hamming_distance(&str_a_bytes, &str_b_bytes).unwrap();
+        assert_eq!(score, expected_score);
+    }
+}
