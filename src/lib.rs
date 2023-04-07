358a0e14310d1ae3bd7730f21a3a7a56b91708bd
Douglas Wu <wckdouglas@gmail.com>
2023-04-07 00:05:19 -0400

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ pub mod models;
 
 use crate::cli::{Command, Parser};
 use crate::distance::hamming_distance;
-use crate::models::{Bcl2FqStats, ConversionResult, DemuxResult};
+use crate::models::{Bcl2FqStats, ConversionResult};
 use log::info;
+use models::UnknownBarcode;
 use serde_json::from_str;
 
 use std::collections::HashMap;
@@ -15,11 +16,24 @@ use std::io::Read;
 const HEADER: &str = "sq_id\tbarcode\tread_count\tpossible_sq_index";
 const MAX_COUNT_UNDETERMINED: usize = 10;
 
+/// Parsing ConversionResult (per lane) from the bcl2fastq json file
+///
+/// # Arguments
+/// - conversion_result: ConversionResult object
+/// - barcode_counter: a hash map collecting the count for each sample
+/// - barcode_list: a hash map collecting the conversion between sample id and barcode
+///
+/// # Returns
+/// - None
 fn collect_lane_barcode_count(
     conversion_result: &ConversionResult,
     barcode_counter: &mut HashMap<String, u64>,
     barcode_list: &mut HashMap<String, String>,
 ) -> Result<(), String> {
+    info!(
+        "Collecting sample barcode count from lane {}",
+        conversion_result.LaneNumber
+    );
     for demux_sample in &conversion_result.DemuxResults {
         let sample_id: &String = &demux_sample.SampleId;
         let barcode: &String = &demux_sample.IndexMetrics[0].IndexSequence;
@@ -35,6 +49,83 @@ fn collect_lane_barcode_count(
         };
 
         barcode_list.insert(sample_id.clone(), barcode.to_string());
+    }
+    Ok(())
+}
+
+fn collect_lane_undetermined_barcode(
+    unknown_barcode_lane: &UnknownBarcode,
+    undetermined_barcode_counter: &mut HashMap<String, u64>,
+) {
+    info!(
+        "Collecting undetermined barcode from lane {}",
+        unknown_barcode_lane.Lane
+    );
+    for (undetermined_barcode, read_count) in unknown_barcode_lane.Barcodes.iter() {
+        match undetermined_barcode_counter.get(undetermined_barcode) {
+            Some(count) => {
+                undetermined_barcode_counter
+                    .insert(undetermined_barcode.clone(), count + read_count);
+            }
+            None => {
+                undetermined_barcode_counter.insert(undetermined_barcode.clone(), *read_count);
+            }
+        };
+    }
+}
+
+fn print_undetermined_barcode(
+    undetermined_barcode_counter: &mut HashMap<String, u64>,
+    barcode_list: &HashMap<String, String>,
+    max_distance: &u8,
+) -> Result<(), String> {
+    let mut sorted_undetermined_barcode_count = Vec::from_iter(undetermined_barcode_counter);
+    sorted_undetermined_barcode_count.sort_by(|(_, a), (_, b)| b.cmp(&a));
+
+    for it in sorted_undetermined_barcode_count.iter().enumerate() {
+        let (i, (undetermined_barcode, barcode_count)) = it;
+        let list_of_possible_barcodes: Vec<String> = barcode_list
+            .iter()
+            .map(|(barcode_id, barcode)| {
+                (
+                    barcode_id,
+                    hamming_distance(barcode.as_bytes(), undetermined_barcode.as_bytes()).unwrap(),
+                )
+            })
+            .filter(|(_barcode_id, score)| score < &max_distance)
+            .map(|(barcode_id, _score)| barcode_id.clone())
+            .collect();
+        let possible_barcodes: String = list_of_possible_barcodes.join(",");
+
+        println!(
+            "Undetermined\t{}\t{}\t{}",
+            undetermined_barcode, barcode_count, possible_barcodes
+        );
+        if i == MAX_COUNT_UNDETERMINED - 1 {
+            return Ok(());
+        }
+    }
+    Ok(())
+}
+
+/// printing out the barcode count
+///
+/// # Arguments
+/// - barcode_counter: hash map of sample id/count
+/// - barcode_list: hash map of sample id/barcode
+fn print_barcode_count(
+    barcode_counter: &HashMap<String, u64>,
+    barcode_list: &HashMap<String, String>,
+) -> Result<(), String> {
+    // print out the barcode counts
+    let mut sorted_barcode_count = Vec::from_iter(barcode_counter);
+    // sort the barcode by counts
+    sorted_barcode_count.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+    for (sample_id, barcode_count) in sorted_barcode_count.iter() {
+        let barcode: &String = barcode_list
+            .get(&(*sample_id).clone())
+            .ok_or(format!("No barcode collected for {}", sample_id))?;
+        println!("{}\t{}\t{}\t", sample_id, barcode, barcode_count);
     }
     Ok(())
 }
@@ -56,44 +147,33 @@ pub fn run() -> Result<(), String> {
     let bcl2fastq_stats: Bcl2FqStats = from_str(&data).unwrap();
 
     // parse demux result from all lanes
-    for conversion_result in &bcl2fastq_stats.ConversionResults {
-        // this is running for each lane
-        collect_lane_barcode_count(conversion_result, &mut barcode_counter, &mut barcode_list)?;
-    }
+    let _ = &bcl2fastq_stats
+        .ConversionResults
+        .into_iter()
+        .map(|conversion_result| {
+            collect_lane_barcode_count(&conversion_result, &mut barcode_counter, &mut barcode_list)
+        })
+        .collect::<Vec<Result<(), String>>>();
 
-    for (sample_id, barcode_count) in barcode_counter.iter() {
-        let barcode: &String = barcode_list
-            .get(sample_id)
-            .ok_or("No barcode collected".to_string())?;
-        println!("{}\t{}\t{}\t{}", sample_id, barcode, barcode_count, "");
-    }
+    print_barcode_count(&barcode_counter, &barcode_list)?;
 
-    for it in bcl2fastq_stats.UnknownBarcodes[0]
-        .Barcodes
+    let mut undetermined_barcode_counter: HashMap<String, u64> = HashMap::new();
+
+    let _ = bcl2fastq_stats
+        .UnknownBarcodes
         .iter()
-        .enumerate()
-    {
-        let (i, (undetermined_barcode, barcode_count)) = it;
-        let list_of_possible_barcodes: Vec<String> = barcode_list
-            .iter()
-            .map(|(barcode_id, barcode)| {
-                (
-                    barcode_id,
-                    hamming_distance(barcode.as_bytes(), undetermined_barcode.as_bytes()).unwrap(),
-                )
-            })
-            .filter(|(_barcode_id, score)| score < &args.max_distance)
-            .map(|(barcode_id, _score)| barcode_id.clone())
-            .collect();
-        let possible_barcodes: String = list_of_possible_barcodes.join(",");
+        .map(|unknown_barcode_for_lane| {
+            collect_lane_undetermined_barcode(
+                unknown_barcode_for_lane,
+                &mut undetermined_barcode_counter,
+            )
+        })
+        .collect::<Vec<_>>();
+    print_undetermined_barcode(
+        &mut undetermined_barcode_counter,
+        &barcode_list,
+        &args.max_distance,
+    )?;
 
-        println!(
-            "Undetermined\t{}\t{}\t{}",
-            undetermined_barcode, barcode_count, possible_barcodes
-        );
-        if i == MAX_COUNT_UNDETERMINED - 1 {
-            return Ok(());
-        }
-    }
     Ok(())
 }
